a03a503818afebee56409c5efb3a14e53c520211
cemilker <88182343+ckaraduman@users.noreply.github.com>
2022-07-14 14:38:28 +0300

    Çalışmalara devam

--- js/FPCem.js
+++ js/FPCem.js
@@ -2,3 +2,7 @@ alert("Merhaba! ");
 document.writeln("Cem İlker Karaduman ");
 document.writeln("MakroPort 2022 ");
 document.writeln("Palmet ");
+var degisken1 = 'Cem İlker Karaduman';
+var deger = degisken1.length;
+document.writeln(degisken1);
+document.writeln("karakter sayısı : "+deger);
