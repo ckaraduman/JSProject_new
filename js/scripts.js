0c93a0e5d9428ec5964256fef9b74fc87e6680bd
cemilker <88182343+ckaraduman@users.noreply.github.com>
2022-07-14 16:03:50 +0300

    PopUp pencere ile giriş hazırlıkları

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@ function myFunction(){
 			 var x = document.getElementById("myInput").value;
  document.getElementById("myP").innerHTML = ("Değerimiz:" + " " + x );
 		 }
+// window.open("pages/cem.html");
+var pencere = window.open("","MakroPort Kullanıcı Giriş Paneli", "width=400,height=400");
+pencere.document.write("<span style='font-weight:bold; font-size:25px;'>"+pencere.name+"</span>"); //pencerenin icerigine yaz
